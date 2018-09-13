9668e230100d4d2f055e188f5865fa73fbe61ea0
Anton Ivanov <anton.aleks.ivanov@gmail.com>
2018-09-13 16:01:12 -0700

    Added some comments

--- app/services/data-service.js
+++ app/services/data-service.js
@@ -3,15 +3,17 @@
 angular.module('myApp.data-service', ['myApp.socket-service'])
     .factory('DataService', ['SocketService', function(SocketService) {
 
-        // Open new socket connection
+        // Open a new socket connection
         SocketService.setHost('dev.sensemetrics.com', 4201);
 
         return {
+
+            // Retrieve all nodes from server
             getNodes: function() {
                 SocketService.addOpenListener(function() {
                     SocketService.sendRequest('getNodes', {}, function(nodes) {
 
-                        // TODO: return data to controller
+                        // TODO: return data to controller for display
                         console.log('Got all nodes:', nodes);
 
                         // Subscribe to each node's update events
@@ -19,7 +21,7 @@ angular.module('myApp.data-service', ['myApp.socket-service'])
                             SocketService.sendRequest('subscribeNodes', {nodes: [nodes[i].id]}, function(newData) {
                                 if (!('code' in newData) && 'type' in newData && 'data' in newData) {
 
-                                    // TODO: return fresh data to controller and keep the list updated
+                                    // TODO: return updated data for a single node to controller and update display
                                     console.log('Got new node data:', newData.data);
                                 }
                             });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,5 +11,7 @@ angular.module('myApp.view1', ['ngRoute', 'myApp.data-service'])
 
 .controller('View1Ctrl', ['DataService', function(DataService) {
 
+  // TODO: get returned array of nodes from the backend, display as a list in the front-end
+  // TODO: data service will periodically receive updates for single nodes, need to update fron-end accordingly
   DataService.getNodes();
 }]);
